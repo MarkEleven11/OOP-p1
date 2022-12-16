70d2b0399dbd1efa3bd7a5c6ccb7aef44545ea0d
Александр <alexakr1993@gmail.com>
2022-12-16 14:35:17 +0400

    People

---
+++ src/Man.java
@@ -0,0 +1,12 @@
+public class Man {
+    public int yearOfBirth;
+    public String name;
+    public String town;
+    public String jobTitle;
+
+
+    public String toString() {
+        return "Привет, меня зовут " + name + ". Я из города  " + town + ". Я родился в " + yearOfBirth + " году.";
+    }
+
+}
